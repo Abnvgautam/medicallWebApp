ac856ba70de41633156499ac3bc6e11cfb7466ca
Abnvgautam <Agautam6854@conestogac.on.ca>
2024-02-09 12:19:13 -0500

    button added

--- frontend/src/pages/specialities/specialities.js
+++ frontend/src/pages/specialities/specialities.js
@@ -102,6 +102,7 @@ const Specialities = () => {
                                 <Chip label="10:00 AM" color="primary" variant="outlined"/>
                             </Stack>
                             </div>
+                            <Button variant="primary" className='btn-booking'>Book Now</Button>
                             
                             
                             
