03a8abf96c1c395a27c1529d4c29ee446164ab19
Abnvgautam <Agautam6854@conestogac.on.ca>
2024-02-11 21:47:43 -0500

    navbar link

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -24,10 +24,10 @@ function Navigation() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto navbar-content " >
-              <Nav.Link as={NavLink} to="/">Home</Nav.Link>
-              <Nav.Link as={NavLink} to="/ourdoctors">Our Doctors</Nav.Link>
-              <Nav.Link as={NavLink} to="/specialities">Specialities</Nav.Link>
-              <Nav.Link as={NavLink} to="/aboutus">About Us</Nav.Link>
+              <Nav.Link as={NavLink} to="/" className='nav-link'>Home</Nav.Link>
+              <Nav.Link as={NavLink} to="/ourdoctors" className='nav-link'>Our Doctors</Nav.Link>
+              <Nav.Link as={NavLink} to="/specialities" className='nav-link'>Specialities</Nav.Link>
+              <Nav.Link as={NavLink} to="/aboutus" className='nav-link'>About Us</Nav.Link>
             </Nav>
             <Nav className="ml-auto buttons">
               <Button variant="outline-primary" className="mx-4 button-style" >Log In</Button>
