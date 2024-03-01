5970042b92f72e89c6476db529ebc84451d89998
Abnvgautam <Agautam6854@conestogac.on.ca>
2024-03-01 15:13:44 -0500

    user routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,21 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const  { getUsers}  = require('../controllers/userController')
+const  { getUsers, setUsers, updateUsers, deleteUsers}  = require('../controllers/userController')
 
 
-router.get('/', getUsers)
+router.route('/').get(getUsers).post(setUsers)
+router.route('/:id').put(updateUsers).delete(deleteUsers)
 
-router.post('/', (req, res) => {
-    res.status(200).json({message: 'Create Users'})
-})
-
-router.put('/:id', (req, res) => {
-    res.status(200).json({message: `Update User ${req.params.id}`})
-})
-
-router.delete('/:id', (req, res) => {
-    res.status(200).json({message: `Delete User ${req.params.id}`})
-})
 
 
 module.exports = router
